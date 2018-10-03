40509c92a7a7f04a234e1df51c4ce18cb2f5f76e
jonylugos <axelkreitler@hotmail.com>
2018-10-02 21:53:59 -0300

    Clase 25

--- AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
@@ -7,10 +7,10 @@ namespace AspNetCoreTodo.Services
 {
     public interface ITodoItemService
     {
-         Task<TodoItem[]> GetIncompleteItemsAsync();
+         Task<TodoItem[]> GetIncompleteItemsAsync(ApplicationUser currentUser);
          
-         Task<bool> AddItemAsync(TodoItem newItem);
+         Task<bool> AddItemAsync(TodoItem newItem, ApplicationUser currentUser);
 
-         Task<bool> MarkDoneAsync(Guid id);
+         Task<bool> MarkDoneAsync(Guid id, ApplicationUser currentUser);
     }
 }
