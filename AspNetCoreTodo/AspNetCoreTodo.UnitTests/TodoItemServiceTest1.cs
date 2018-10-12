597bb891c8540c0b935afcffa8ff2972e5f8c0f0
jonylugos <axelkreitler@hotmail.com>
2018-10-11 21:58:59 -0300

    Clase 28

---
+++ AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceTest1
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            
+        }
+    }
+}
